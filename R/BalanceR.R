e83294a0ef3c2bb29f4be516776b91b2af4b4a68
Jaehyun Song <tintstyle@gmail.com>
2019-05-07 19:37:29 +0900

    First Upload

---
+++ R/BalanceR.R
@@ -0,0 +1,160 @@
+SB_Calc_C  <- function(x, y) {
+    mx <- mean(x, na.rm = TRUE)
+    my <- mean(y, na.rm = TRUE)
+    vx <- var(x, na.rm = TRUE)
+    vy <- var(x, na.rm = TRUE)
+
+    return(100 * (mx - my) / sqrt(0.5 * (vx + vy)))
+}
+
+SB_Calc_B  <- function(x, y) {
+    mx <- mean(x, na.rm = TRUE)
+    my <- mean(y, na.rm = TRUE)
+
+    return(100 * (mx - my) / sqrt(0.5 * (mx * (1 - mx) + my * (1 - my))))
+}
+
+BalanceR <- function(data, group, cov) {
+    Group    <- as.character(sort(unique(data[, group])))
+    NGroup   <- length(Group)
+    GrpComb  <- combn(Group, 2)
+    NComb    <- ncol(GrpComb)
+    NCov     <- length(cov)
+
+    CombName <- apply(combn(Group, 2), 2, paste, collapse = "-")
+
+    Result.df <- as.data.frame(matrix(rep(NA,
+                                          NCov * (NGroup * 2 + NComb) + NCov) ,
+                                      nrow = NCov))
+
+    # Descriptive Statistics
+    SumText <- paste0(rep(c("mean(", "sd("), NCov),
+                      rep(cov, each = 2),
+                      rep(")", NCov * 2))
+
+    DS <- data %>%
+        group_by(Group = eval(parse(text = group))) %>%
+        summarise(!!!parse_exprs(SumText)) %>%
+        select(-Group) %>%
+        as.data.frame()
+
+    DSm <- DS %>%
+        select(starts_with("mean")) %>%
+        t() %>%
+        as.data.frame()
+    names(DSm) <- paste0("V", seq(1, NGroup * 2, 2))
+
+    DSs <- DS %>%
+        select(starts_with("sd")) %>%
+        t() %>%
+        as.data.frame()
+    names(DSs) <- paste0("V", seq(2, NGroup * 2, 2))
+
+    DS <- cbind(DSm, DSs)
+
+    DS <- DS %>%
+        select(!!!parse_exprs(sort(names(DS))))
+
+    Result.df[1:NCov, 2:(NGroup * 2 + 1)] <- DS
+
+    # Standardized Bias
+    tmpSB <- as.data.frame(matrix(rep(0, NCov * NComb),
+                                  nrow = NCov))
+
+    for (i in 1:NCov) {
+        tmpCov <- cov[i]
+        for (j in 1:NComb) {
+            tmpG1  <- GrpComb[1, j]
+            tmpG2  <- GrpComb[2, j]
+
+            G1Text <- paste0("data %>% filter(",
+                             group, " == '", tmpG1, "') %>% ",
+                             ".$", tmpCov)
+            G2Text <- paste0("data %>% filter(",
+                             group, " == '", tmpG2, "') %>% ",
+                             ".$", tmpCov)
+
+            G1 <- eval(parse(text = G1Text))
+            G2 <- eval(parse(text = G2Text))
+
+            if (length(unique(c(G1, G2))) == 2 &
+                 (0 %in% c(1, 0)) == TRUE &
+                 (1 %in% c(1, 0)) == TRUE) {
+                tmpSB[i, j] <- SB_Calc_B(G1, G2)
+            }else{
+                tmpSB[i, j] <- SB_Calc_C(G1, G2)
+            }
+
+        }
+    }
+
+    Result.df[1:NCov, (NGroup * 2 + 2):ncol(Result.df)] <- tmpSB
+
+    names(Result.df) <- c("Covariate",
+                          paste0(rep(c("Mean:", "SD:"), NGroup),
+                                 rep(Group, each = 2)),
+                          paste0("SB:", CombName))
+
+    Result.df$Covariate <- cov
+    Result.df$Covariate <- fct_rev(fct_inorder(Result.df$Covariate))
+
+
+
+    Result <- list(Desc = Result.df[, 1:(NGroup * 2 + 1)],
+                   SB   = Result.df[, c(1, (NGroup * 2 + 2):ncol(Result.df))])
+
+    class(Result) <- append("BalanceR", class(Result))
+
+    return(Result)
+}
+
+print.BalanceR <- function(data, only.SB = FALSE, digits = 3) {
+
+    if (only.SB == FALSE) {
+        x <- data$Desc
+        y <- data$SB
+        x <- cbind(x, y[, -1])
+    }else{
+        x <- data$SB
+    }
+
+    x[1:nrow(x), 2:ncol(x)] <- format(round(x[1:nrow(x), 2:ncol(x)], digits),
+                                      nsmall = digits)
+
+    print(x)
+}
+
+plot.BalanceR <- function(data,
+                          point.size = 2.5,
+                          text.size = 12,
+                          vline = c(3, 5, 10)) {
+    x <- data$SB
+    x <- x %>%
+        select(Covariate, starts_with("SB")) %>%
+        gather(key = Pair, value = SB, -Covariate) %>%
+        separate(Pair, into = c("X", "Pair"), sep = ":") %>%
+        select(-X)
+
+    plot_x <- x %>%
+        ggplot(aes(x = SB, y = Covariate, color = Pair)) +
+        geom_vline(xintercept = c(-vline, vline),
+                   linetype = 2) +
+        geom_vline(xintercept = 0) +
+        geom_point(size = point.size) +
+        scale_x_continuous(breaks = c(-vline, 0, vline),
+                           labels = c(-vline, 0, vline)) +
+        labs(x = "Standardized Bias", color = "") +
+        theme_bw() +
+        theme(legend.position = "bottom",
+              text = element_text(size = text.size))
+
+    print(plot_x)
+}
+
+Test.df %>%
+    BalanceR(group = "Group", cov = c("Sex", "Educ")) %>%
+    print(only.SB = TRUE)
+
+Test.df %>%
+    BalanceR(group = "Group", cov = c("Sex", "Educ")) %>%
+    plot()
