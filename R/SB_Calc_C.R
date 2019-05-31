9d9078d3a47e4a913348202ee07743815ef27cef
Jaehyun Song <tintstyle@gmail.com>
2019-05-31 18:25:13 +0900

    Delete SB_Calc_C.R

--- R/SB_Calc_C.R
+++
@@ -1,8 +0,0 @@
-SB_Calc_C  <- function(x, y) {
-    mx <- mean(x, na.rm = TRUE)
-    my <- mean(y, na.rm = TRUE)
-    vx <- var(x, na.rm = TRUE)
-    vy <- var(x, na.rm = TRUE)
-
-    return(100 * (mx - my) / sqrt(0.5 * (vx + vy)))
-}
